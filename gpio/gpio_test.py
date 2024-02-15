12a9775b471e3a8b4513e9c45ecff60420566b7f
JordanDeitsch <jordan.b.deitsch@gmail.com>
2024-02-14 23:25:26 -0700

    Fixing Python spaces over tabs in all files in repo

--- gpio/gpio_test.py
+++ gpio/gpio_test.py
@@ -17,20 +17,20 @@ currentState = GPIO.input(pinButton)
 prevState = GPIO.input(pinButton)
 
 while True:
-	if(GPIO.input(pinButton)) == 1:
-		currentState = 1
-	else:
-		currentState = 0
-		
-	if((currentState != prevState) and (currentState == 0)):
-		print("Button Pressed")
-		GPIO.output(pinLED, GPIO.HIGH)
-		time.sleep(.1)
-	elif((currentState != prevState) and (currentState == 1)):
-		print("Button Released")
-		GPIO.output(pinLED, GPIO.LOW)
-		time.sleep(.1)
-	
-	prevState = currentState
+    if(GPIO.input(pinButton)) == 1:
+        currentState = 1
+    else:
+        currentState = 0
+
+    if((currentState != prevState) and (currentState == 0)):
+        print("Button Pressed")
+        GPIO.output(pinLED, GPIO.HIGH)
+        time.sleep(.1)
+    elif((currentState != prevState) and (currentState == 1)):
+        print("Button Released")
+        GPIO.output(pinLED, GPIO.LOW)
+        time.sleep(.1)
+
+    prevState = currentState
 
 GPIO.cleanup()
