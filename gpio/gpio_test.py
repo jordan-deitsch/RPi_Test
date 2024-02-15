b945216331e697c75bea2a490785327e9a046d9f
JordanDeitsch <jordan.b.deitsch@gmail.com>
2024-02-14 21:45:33 -0700

    Basic IO functionality test of RPi pins with button and LED

--- gpio/gpio_test.py
+++ gpio/gpio_test.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
 
-import gpiozero
+import RPi.GPIO as GPIO
 import time
 
-print("Hello {} \n".format("Jordan"))
+pinLED = 17
+pinButton = 22
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pinLED, GPIO.OUT)	# LED output
+GPIO.setup(pinButton, GPIO.IN)	# Button input
+
+
+currentState = GPIO.input(pinButton)
+prevState = GPIO.input(pinButton)
+
+while True:
+	if(GPIO.input(pinButton)) == 1:
+		currentState = 1
+	else:
+		currentState = 0
+		
+	if((currentState != prevState) and (currentState == 0)):
+		print("Button Pressed")
+		GPIO.output(pinLED, GPIO.HIGH)
+		time.sleep(.1)
+	elif((currentState != prevState) and (currentState == 1)):
+		print("Button Released")
+		GPIO.output(pinLED, GPIO.LOW)
+		time.sleep(.1)
+	
+	prevState = currentState
+
+GPIO.cleanup()
